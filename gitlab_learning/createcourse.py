ed84d87d8d22e2c6721eef4bee04912a44acb3b3
Magnus Hagdorn <magnus.hagdorn@charite.de>
2025-03-14 16:25:09 +0100

    create personal projects

--- gitlab_learning/createcourse.py
+++ gitlab_learning/createcourse.py
@@ -1,4 +1,5 @@
 from .gl import GitlabLearning
+import gitlab
 import yaml
 from pathlib import Path
 import argparse
@@ -19,11 +20,32 @@ def main():
     gl = GitlabLearning(gitlab_id=gitlab_id)
 
     course_group = gl.get_group(config["name"], create=True)
-    year_group = gl.get_group(config["year"], parent_group=course_group,
-                              create=True)
+    year_group = gl.get_group(
+        config["year"], parent_group=course_group, create=True)
+    personal_group = gl.get_group(
+        "personal", parent_group=year_group, create=True)
 
-    print(course_group)
-    print(year_group)
+    # handle personal projects
+    projects = {}
+    for p in personal_group.projects.list():
+        projects[p.name] = p
+
+    for u in config['students']:
+        if u not in projects:
+            projects[u] = gl.gl.projects.create(
+                {'name': u, 'namespace_id': personal_group.id})
+        project = gl.gl.projects.get(projects[u].id)
+
+        user = gl.getUser(u)
+        if user is not None:
+            try:
+                member = project.members.get(user.id)
+            except gitlab.exceptions.GitlabGetError:
+                member = project.members.create(
+                    {'user_id': user.id, 'access_level':
+                     gitlab.const.AccessLevel.MAINTAINER})
+
+        # need to add some files to repo
 
 
 if __name__ == '__main__':
