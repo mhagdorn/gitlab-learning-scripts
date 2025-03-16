068e28d4fb13c66ec1e90d4c8a34e54f7df81a98
Magnus Hagdorn <magnus.hagdorn@charite.de>
2025-03-16 13:32:42 +0100

    create attendance sheet

---
+++ gitlab_learning/createattendance.py
@@ -0,0 +1,66 @@
+from .gl import GitlabLearning
+import yaml
+from pathlib import Path
+import argparse
+from jinja2 import Environment, PackageLoader, select_autoescape
+import datetime
+
+
+def format_date(value):
+    if isinstance(value, datetime.date):
+        dt = value
+    else:
+        dt = datetime.date.fromisoformat(value)
+    return dt.strftime("%A, %d %B %Y")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("course", type=Path,
+                        help="the course description file")
+    parser.add_argument("-s", "--session", type=int,
+                        help="the session number")
+    parser.add_argument("-o", "--output", type=Path,
+                        help="write to results to file")
+    args = parser.parse_args()
+
+    config = yaml.safe_load(args.course.read_text())
+
+    env = Environment(
+        loader=PackageLoader("gitlab_learning"),
+        autoescape=select_autoescape())
+    env.filters["datetime"] = format_date
+
+    if 'gitlab' in config:
+        gitlab_id = config['gitlab']
+    else:
+        gitlab_id = None
+    gl = GitlabLearning(gitlab_id=gitlab_id)
+
+    users = []
+    for u in config['students']:
+        user = gl.getUser(u)
+        if user is not None:
+            name = user.name
+        else:
+            name = None
+        users.append((name, u))
+
+    if args.session is not None:
+        try:
+            session = config["sessions"][args.session-1]
+        except Exception as e:
+            parser.error(e)
+        sign_in = env.get_template("sign-in.tex")
+        out = sign_in.render(**config, users=users, session=session)
+    else:
+        attendance = env.get_template("attendance.csv")
+        out = attendance.render(**config, users=users)
+    if args.output is None:
+        print(out)
+    else:
+        args.output.write_text(out)
+
+
+if __name__ == "__main__":
+    main()
