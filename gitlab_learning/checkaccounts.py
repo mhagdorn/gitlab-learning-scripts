244d21ca338ff603699ec4046ed77d0569756434
Magnus Hagdorn <magnus.hagdorn@charite.de>
2025-03-17 09:05:49 +0100

    also print whether users have a personal project

--- gitlab_learning/checkaccounts.py
+++ gitlab_learning/checkaccounts.py
@@ -18,11 +18,20 @@ def main():
         gitlab_id = None
     gl = GitlabLearning(gitlab_id=gitlab_id)
 
-    print("user status hasKeys")
+    course_group = gl.get_group(config["name"])
+    year_group = gl.get_group(config["year"], parent_group=course_group)
+    personal_group = gl.get_group("personal", parent_group=year_group)
+
+    personal_projects = {}
+    for p in personal_group.projects.list():
+        personal_projects[p.name] = p
+
+    print("user status hasKeys hasPersonal")
     for u in config['students']:
         user = gl.getUser(u)
         if user is not None:
-            print(user.username, user.state, len(user.keys.list()) > 0)
+            print(user.username, user.state, len(user.keys.list()) > 0,
+                  u in personal_projects)
         else:
             print(u)
 
