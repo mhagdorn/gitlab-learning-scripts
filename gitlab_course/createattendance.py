978d7acaa7a68656fa9ca73051e4359d3bbc276c
Magnus Hagdorn <magnus.hagdorn@charite.de>
2025-06-04 17:45:57 +0200

    rename program

--- gitlab_course/createattendance.py
+++ gitlab_course/createattendance.py
@@ -20,8 +20,6 @@ def main():
                         help="the course description file")
     parser.add_argument("-s", "--session", type=int,
                         help="the session number")
-    parser.add_argument("-l", "--lecture", type=int,
-                        help="the session number")
     parser.add_argument("-o", "--output", type=Path,
                         help="write to results to file")
     args = parser.parse_args()
@@ -40,7 +38,7 @@ def main():
     glc = GitlabCourse(gitlab_id=gitlab_id)
 
     users = []
-    for u in config['students']:
+    for u in config['participants']:
         user = glc.getUser(u)
         if user is not None:
             users.append((user.name, user.username))
@@ -54,16 +52,10 @@ def main():
             parser.error(e)
         sign_in = env.get_template("sign-in.tex")
         out = sign_in.render(**config, users=users, session=session)
-    elif args.lecture is not None:
-        try:
-            session = config["sessions"][args.lecture-1]
-        except Exception as e:
-            parser.error(e)
-        lecture = env.get_template("session.tex")
-        out = lecture.render(**config, session=session)
     else:
         attendance = env.get_template("attendance.csv")
         out = attendance.render(**config, users=users)
+
     if args.output is None:
         print(out)
     else:
