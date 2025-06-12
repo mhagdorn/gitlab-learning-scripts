25a0715fbf0e65d6d668916c65eb1ece529607f7
Magnus Hagdorn <magnus.hagdorn@charite.de>
2025-06-12 13:31:06 +0200

    mock some packages

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+autodoc_mock_imports = [
+    "gitlab"]
+
 from gitlab_course.__version__ import __version__
 
 # -- Project information -----------------------------------------------------
